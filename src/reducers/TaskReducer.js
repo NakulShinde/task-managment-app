756685b4c03c97f845e9557efd0e02f05d7cc4f7
NakulShinde <nakulshinde.it@gmail.com>
2018-11-04 11:21:34 +0530

    tasks orderby dueData & priority

--- src/reducers/TaskReducer.js
+++ src/reducers/TaskReducer.js
@@ -11,30 +11,48 @@ const getcurrentPageTasks = (tasks, currentPage) => {
     let highIndex = (tasks.length < PAGE_LIMIT)
         ? tasks.length
         : (currentPage + 1) * PAGE_LIMIT;
-        
+
     return [...tasks.slice(currentPage * PAGE_LIMIT, highIndex)];
 }
 
-export default function (state = defaultState, action) {
+const orderByDueDate = (tasks) => {
+    return tasks.sort((firstTask, secTask) => {
+        return firstTask.dueDate - secTask.dueDate
+    });
+}
+const orderByPriority = (tasks) => {
+    return tasks.sort((firstTask, secTask) => {
+        return secTask.priority - firstTask.priority
+    });
+}
 
+const orderByDueDateAndPriority = (tasks) => {
+    return orderByPriority(orderByDueDate(tasks));
+}
+
+export default function (state = defaultState, action) {
+    let orderBy = null;
     switch (action.type) {
         case 'FETCH_TASKLIST_DATA':
             return Object.assign(state, {isLoading: true});
         case 'SET_TASKLIST_DATA':
+
+            orderBy = orderByDueDateAndPriority(action.payload);
+
             return Object.assign({}, {
                 isLoading: false,
                 currentPage: 1,
-                tasks: [...action.payload],
-                currentPageTasks: getcurrentPageTasks(action.payload, 1)
+                tasks: [...orderBy],
+                currentPageTasks: getcurrentPageTasks(orderBy, 1)
             });
-
-        case 'UPDATE_TASKLIST_DATA':
+            
+            case 'UPDATE_TASKLIST_DATA':
+            
+            orderBy = orderByDueDateAndPriority(...state.tasks);
+    
             return {
                 isLoading: false,
-                tasks: [
-                    ...state,
-                    action.payload
-                ]
+                tasks: [...orderBy]
             }
         case 'ERROR_TASKLIST_DATA':
             return {
