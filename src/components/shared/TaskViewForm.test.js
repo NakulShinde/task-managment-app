39e74ead8a1c284916876d7f12e4093140dc16c4
NakulShinde <nakulshinde.it@gmail.com>
2018-11-11 18:25:44 +0530

    Test cases alignment

--- src/components/shared/TaskViewForm.test.js
+++ src/components/shared/TaskViewForm.test.js
@@ -1,47 +1,47 @@
-import React from 'react';
-import {mount} from 'enzyme';
-import configureStore from 'redux-mock-store' //ES6 modules
-import thunk from 'redux-thunk';
-import renderer from 'react-test-renderer';
-import {MemoryRouter} from "react-router-dom";
+import React from "react";
+import { mount } from "enzyme";
+import configureStore from "redux-mock-store"; //ES6 modules
+import thunk from "redux-thunk";
+import renderer from "react-test-renderer";
+import { MemoryRouter } from "react-router-dom";
 
 const store = configureStore([thunk])();
 
-import TaskViewForm from './TaskViewForm';
+import TaskViewForm from "./TaskViewForm";
 
-it('renders TaskViewForm snapshot correctly', () => {
+it("renders TaskViewForm snapshot correctly", () => {
     const tree = renderer
         .create(
-        <MemoryRouter>
-            <TaskViewForm store={store}/>
-        </MemoryRouter>
-    )
+            <MemoryRouter>
+                <TaskViewForm store={store} />
+            </MemoryRouter>
+        )
         .toJSON();
     expect(tree).toMatchSnapshot();
 });
 
 const myWrapper = mount(
     <MemoryRouter>
-        <TaskViewForm store={store}/>
+        <TaskViewForm store={store} />
     </MemoryRouter>
 ).find(TaskViewForm);
 
-it('render a TaskViewForm', () => {
-    expect(myWrapper.exists()).toBe(true)
-})
+it("render a TaskViewForm", () => {
+    expect(myWrapper.exists()).toBe(true);
+});
 
-it('render 10 formRow fields, 3 Input fields & 2 buttons', () => {
-    expect(myWrapper.find('.formRow').length).toBe(10)
-    expect(myWrapper.find('input').length).toBe(3)
-    expect(myWrapper.find('button').length).toBe(2)
-})
+it("render 10 formRow fields, 3 Input fields & 2 buttons", () => {
+    expect(myWrapper.find(".formRow").length).toBe(10);
+    expect(myWrapper.find("input").length).toBe(3);
+    expect(myWrapper.find("button").length).toBe(2);
+});
 
-it('render a form fields', () => {
-    expect(myWrapper.find('form').exists()).toBe(true)
-    expect(myWrapper.find('#title').exists()).toBe(true)
-    expect(myWrapper.find('#description').exists()).toBe(true)
-    expect(myWrapper.find('#priority').exists()).toBe(true)
-})
+it("render a form fields", () => {
+    expect(myWrapper.find("form").exists()).toBe(true);
+    expect(myWrapper.find("#title").exists()).toBe(true);
+    expect(myWrapper.find("#description").exists()).toBe(true);
+    expect(myWrapper.find("#priority").exists()).toBe(true);
+});
 /*
 it('field change checks', () => {
     const titleField = myWrapper.find('form')
